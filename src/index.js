ccc3208ed4c70c8a5b69693589f7014f9bef5700
Eric Moynihan <git@moynihan.io>
2019-12-14 14:48:35 +0000

    DOne

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
 export default class extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { index: 1 };
+    this.click = this.click.bind(this); // Allows click access to this when called by the button
+  }
+  otherRender(index) {
+    switch (index) {
+      case 1:
+        return <h1>Page 1 boiii</h1>;
+      case 2:
+        return (
+          <div>
+            <p>Lorem ipsum</p>
+          </div>
+        );
+      default:
+        return <b>NOOOB</b>;
+    }
+  }
+  click() {
+    let state = this.state.index + 1;
+    if (this.props.noLorem && state == 2) {
+      state = 3;
+    }
+    this.setState({ index: state });
+  }
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div>
+        <h2>{this.props.title ? this.props.title : "No title passed"}</h2>
+        <button onClick={this.click}>CLIKK PLS</button>
+        <br></br>
+        {this.otherRender(this.state.index)}
+      </div>
+    );
   }
 }
